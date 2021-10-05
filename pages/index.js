8eb53d950efb8b5a0503d0a5496fbbae66ad21bf
Alexandre Desroches <alex.desroches7@gmail.com>
2021-10-05 08:56:24 -0400

    Improve front page content and tweak header + footer (again)

--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,22 @@ export default function Index() {
     >
       <div className="readable-width-container">
         <h1>Alexandre Desroches</h1>
-        <h2>Programmeur de logiciels, applications et sites webs</h2>
+        <h2>Programmeur de logiciels, applications webs et sites internets.</h2>
         <p>
-          Je suis un travailleur autonome qui offre des services de programmation, conception d'applications webs et
-          de sites internets. Je suis disponible, à la demande, pour vos projets.
+          Je suis un passionné de code.
+          En tant que travailleur autonome, j'offre des services personnalisés de programmation pour répondre aux
+          besoins de vos projets.
         </p>
-        <h3>Programmeur indépendant (freelancer)</h3>
+        <h3>Programmation à la demande (freelance)</h3>
         <p>
-          Mes forces en programmation sont surtout pour les langages&nbsp;: Javascript, HTML, CSS. Dans cet écosystème,
-          je maîtrise des technologies modernes en forte demande telles que&nbsp;: React.js, Next.js, Electron.js et
-          Firebase.
+          Mes forces en programmation, sont basées sur les fondements des langages du web. C'est-à-dire,
+          Javascript, HTML et CSS.
         </p>
-        <Link href="/programmation"><a className="text-link">Services de programmation&nbsp;&rarr;</a></Link>
+        <p>
+          Dans ce vaste écosystème, je maîtrise des technologies modernes en forte demande telles que&nbsp;: Node.js,
+          React.js, Next.js, Electron.js et Firebase.
+        </p>
+        <Link href="/programmation"><a className="text-link">Services de programmation freelance&nbsp;&rarr;</a></Link>
       </div>
     </PageLayout>
   );

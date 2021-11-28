c1f5f53470161b3614435713aeb0ac989d2da767
Alexandre Desroches <alex.desroches7@gmail.com>
2021-11-28 11:45:11 -0500

    Added wave background in programmation.js and fixed header vertical centering on mobile devices.

--- pages/programmation.js
+++ pages/programmation.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import PageTemplate from "../components/PageTemplate";
 import ResponsiveImage from "../components/ResponsiveImage";
 import styles from "../styles/programmation.module.css";
+import ExternalLink from "../components/ExternalLink";
 
 export default function Programmation() {
   return (
@@ -28,36 +29,116 @@ export default function Programmation() {
           <h3>Langages et technologies maîtrisées&nbsp;:</h3>
           <div className={styles.techListsContainer}>
             <ul>
-              <li>Javascript</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Node.js</li>
-              <li>JSON</li>
-              <li>Autohotkey</li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/JavaScript">
+                  Javascript
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/HTML">
+                  HTML
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/CSS">
+                  CSS
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Node.js">
+                  Node.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/React_(JavaScript_library)">
+                  React.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Next.js"
+                >Next.js
+                </ExternalLink>
+              </li>
             </ul>
             <ul>
-              <li>React.js</li>
-              <li>Next.js</li>
-              <li>Electron.js</li>
-              <li>Firebase</li>
-              <li>Wordpress</li>
-              <li>WooCommerce</li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Electron_(software_framework)">
+                  Electron.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Firebase">
+                  Firebase
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/AutoHotkey">
+                  Autohotkey
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/JSON">
+                  JSON
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/WordPress">
+                  Wordpress
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/WooCommerce">
+                  WooCommerce
+                </ExternalLink>
+              </li>
             </ul>
           </div>
 
-          <h3>Connaissances de base niveau fonctionnel&nbsp;:</h3>
+          <h3>Connaissances de base&nbsp;:</h3>
           <div className={styles.techListsContainer}>
             <ul>
-              <li>PHP</li>
-              <li>Java</li>
-              <li>C#</li>
-              <li>Python</li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/PHP">
+                  PHP
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Java">
+                  Java
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)">
+                  C#
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Python_(programming_language)">
+                  Python
+                </ExternalLink>
+              </li>
             </ul>
             <ul>
-              <li>Angular.js</li>
-              <li>Vue.js</li>
-              <li>Astro.js</li>
-              <li>Go</li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Angular_(web_framework)">
+                  Angular.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Vue.js">
+                  Vue.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://astro.build/">
+                  Astro.js
+                </ExternalLink>
+              </li>
+              <li>
+                <ExternalLink url="https://en.wikipedia.org/wiki/Go_(programming_language)">
+                  Go
+                </ExternalLink>
+              </li>
             </ul>
           </div>
         </div>
@@ -77,17 +158,31 @@ export default function Programmation() {
         </div>
       </section>
 
+      <svg className={styles.waveTop} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 100">
+        <path fill="var(--main-background-color)" d="M0 0h900v100H0z"/>
+        <path fill="var(--blue)"
+              d="m0 25 37.5 9.5C75 44 150 63 225 65s150-13 225-19.8c75-6.9 150-5.5 225 .5s150 16.6 187.5 22L900 73v28H0Z"/>
+      </svg>
+
       <section className={styles.services}>
         <h3>Ce que je fais</h3>
         <p className="max-text-width">
-          Programmation pour la création et la maintenance de vos projets TI avec une
+          Programmation pour la création et la maintenance de vos projets TI, avec une
           expertise particulière dans les domaines suivants&nbsp;:
         </p>
         <h4>Sites internet</h4>
         <h4>Applications web</h4>
+        <h4>Commerces en ligne</h4>
         <h4>Logiciels pour ordinateurs</h4>
+        <h4>Web scraping</h4>
         <h4>Scripts d'automatisation</h4>
       </section>
+
+      <svg className={styles.waveBottom} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 900 100">
+        <path fill="var(--main-background-color)" d="M0 0h900v100H0z"/>
+        <path fill="var(--blue)"
+              d="m0 31 37.5 5.5C75 42 150 53 225 58.2c75 5.1 150 4.5 225-4s150-24.9 225-21.5C750 36 825 59 862.5 70.5L900 82V0H0Z"/>
+      </svg>
 
       <section className="projects max-content-width display-flex">
         <div className="max-text-width">

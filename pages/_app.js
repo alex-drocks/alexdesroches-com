9a7a4011bc9ab3ad8f547212805d6fb6da3738cb
Alexandre Desroches <alex.desroches7@gmail.com>
2021-12-07 21:13:23 -0500

    Fixed typos and did minor text revisions. Fixed gtag handling exception in dev mode.

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,14 @@ function MyApp({Component, pageProps, nonce}) {
   const router = useRouter()
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+    if (process?.env?.NODE_ENV !== "development") {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+      }
     }
   }, [router.events])
 

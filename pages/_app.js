885de989b16b341c523b81d2b34ea9da9f3cebe1
Alex <alex.desroches7@gmail.com>
2026-04-23 13:26:25 +0000

    chore: remove dead Google Analytics code and comments
    - Remove orphaned gtag imports and commented-out re-enable instructions
      from pages/_app.js.
    - Delete lib/gtag.js (no longer imported anywhere).
    - Clean up redundant zero-value comments that documented removed code.
    
    Co-authored-by: Hermes <hermes-agent@noreply.local>

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,6 @@ import '../styles/Header.css';
 import '../styles/Footer.css';
 
 function MyApp({Component, pageProps}) {
-  // NOTE: Google Analytics is currently disabled. To re-enable:
-  // import {useEffect} from "react";
-  // import {useRouter} from "next/router";
-  // import Script from "next/script";
-  // import * as gtag from '../lib/gtag';
-
   return (
     <>
       <ThemeProvider defaultTheme="light" attribute="class" disableTransitionOnChange>

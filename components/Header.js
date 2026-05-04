48b37f565f2224a24d42221715850eedd1304b03
Alexandre Desroches <alex.desroches7@gmail.com>
2026-05-04 14:48:34 -0400

    fix: update light mode icon styles for improved visibility and consistency

--- components/Header.js
+++ components/Header.js
@@ -181,9 +181,9 @@ function ToggleThemeColorsButton({className = "", shouldDisplayText = false}) {
           </div>
         ) : (
           <div>
-            <svg className="turn-on-light-mode" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"/>
+            <svg className="turn-on-light-mode" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+              <path
+                d="M565-395q35-35 35-85t-35-85q-35-35-85-35t-85 35q-35 35-35 85t35 85q35 35 85 35t85-35Zm-226.5 56.5Q280-397 280-480t58.5-141.5Q397-680 480-680t141.5 58.5Q680-563 680-480t-58.5 141.5Q563-280 480-280t-141.5-58.5ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/>
             </svg>
             {shouldDisplayText && <span>{isEnglish ? <>Light&nbsp;theme</> : <>Thème&nbsp;clair</>}</span>}
           </div>

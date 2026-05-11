1a1ffe70472b6abe00f0740b674b2ba5b7b628c8
Alexandre Desroches <alex.desroches7@gmail.com>
2026-05-11 16:41:01 -0400

    Remove PixelAdventurer component and associated styles from contact pages

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,6 @@ import {EmailLogo, LinkedinLogo} from "../components/Logos";
 
 import {myContactLinks} from "../lib/myContactLinks";
 import ContactLink from "../components/ContactLink";
-import PixelAdventurer from "../components/PixelAdventurer";
 
 import styles from "../styles/contact.module.css";
 
@@ -37,8 +36,6 @@ export default function Contact() {
               svgIcon={<LinkedinLogo/>}
             />
           </ul>
-
-          <PixelAdventurer/>
 
         </section>
 
--- pages/en/contact.js
+++ pages/en/contact.js
@@ -4,7 +4,6 @@ import ContactLink from "../../components/ContactLink";
 import {myContactLinks} from "../../lib/myContactLinks";
 import {EmailLogo, LinkedinLogo} from "../../components/Logos";
 import ResponsiveImage from "../../components/ResponsiveImage";
-import PixelAdventurer from "../../components/PixelAdventurer";
 
 const Contact = () => {
   return (
@@ -34,8 +33,6 @@ const Contact = () => {
               svgIcon={<LinkedinLogo/>}
             />
           </ul>
-
-          <PixelAdventurer/>
 
         </section>
 
